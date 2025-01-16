4aa6ff55a9958727f85a72fb889fa843f1864029
TichNikit <168722731+TichNikit@users.noreply.github.com>
2025-01-16 18:25:41 +0300

    Delete tg directory

--- tg/keybord/kb.py
+++
@@ -1,6 +0,0 @@
-from aiogram import types
-
-keyboard_start = [
-    [types.InlineKeyboardButton(text="Получить текущие данные", callback_data='data')],
-    [types.InlineKeyboardButton(text="Полудить данные из базы данных", callback_data='data_bd')], ]
-keyboard_start = types.InlineKeyboardMarkup(inline_keyboard=keyboard_start)
